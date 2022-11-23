47390fbfba0f105d9630c3f195b7996e6c5c115e
Jeremy Cinq-Mars <jeremy@cinqmars.ca>
2022-11-23 13:38:13 -0500

    Create App

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const port = 8000;
+
+express()
+
+.use(express.json())
+.use(helmet())
+.use(morgan('tiny'))
+
+//start servers endpoints here
+.get('/hello', (req, res) => {
+    res.status(200).json({status: 200, message: "Hello World!"})
+})
+
+.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
